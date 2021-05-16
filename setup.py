00fa46c26f432de2a8f885849c02be344f573caa
sergio2526 <sergiorubiano25@hotmail.com>
2021-05-16 16:49:18 -0500

    Agregando atributo times al metodo convert

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="clisya",
     packages=find_packages(include=['clisya']),
-    version="0.1.0",
+    version="0.1.1",
     description="flags library",
     long_description_content_type="text/markdown",
     author="Sergio Rubiano",
